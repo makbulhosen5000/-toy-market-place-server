ed256362838d1eb2c9cd4d46f4a9f2d3b6bcb3b7
mh akash <mhakash5000@gmail.com>
2023-05-18 17:12:58 +0600

    added database configaration

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
@@ -10,6 +11,35 @@ app.use(express.json())
 
 
 
+//toy
+//tEF0LqcVUiYHCvgs
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.ahuevic.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Toy Market App Server is Running')
