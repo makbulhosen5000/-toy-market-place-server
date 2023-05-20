b022c3b90ceeb7e5f8fb0e1e5fb27652765257b6
mh akash <mhakash5000@gmail.com>
2023-05-20 22:57:40 +0600

    added update method for taking id

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ async function run() {
       res.send(result);
     })
 
+    //taking id for update
+    app.get('/toys:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    })
+
     // for delete data
     app.delete('/toys/:id',async(req,res)=>{
       const id = req.params.id;
